07a70aee184aef712b20c42e9747e6444f65bc45
Yashwardhan singh thakur <yashwardhansinghthakur881@gmail.com>
2025-03-22 16:47:47 +0530

    responsiveness improved

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 let main = document.querySelector(".main");
 let searchInput = document.querySelector(".search-con");
 let hiddenBtns = document.querySelectorAll(".search-con button");
+let expandBtn = document.querySelector(".fa-bars ");
+let expandableBar = document.querySelector(".expandBar");
+let contractBtn = document.querySelector(".fa-x");
+
+let expandableBarSlider = () => {
+  expandableBar.classList.toggle("expandStyle");
+};
+
+expandBtn.addEventListener("click", expandableBarSlider);
+contractBtn.addEventListener("click", expandableBarSlider);
 
 searchInput.addEventListener("input", () => {
   if (this.value !== "") {
